f9c6981cbdf6a227056093b7930b7b65be1da8c0
kevin <kevinwon30@gmail.com>
2024-07-18 21:21:08 -0700

    Completed CSS Modules Tutorial

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import AddToCart from './AddToCart'
+import styles from './ProductCard.module.css' // styles is an JS object with class names as keys
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
       <AddToCart />
     </div>
   )
