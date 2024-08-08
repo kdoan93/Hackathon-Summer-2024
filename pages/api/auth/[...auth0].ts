38c8239c000f49102230bdaa9db9f2263fa648f2
kevin <kevinwon30@gmail.com>
2024-08-08 13:29:52 -0700

    Fix build failure

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,6 +1,13 @@
 // import { handleAuth } from '@auth0/nextjs-auth0';
 
 // export default handleAuth();
-export default (req, res) => {
+
+// Build fails with this code
+// export default (req, res) => {
+//   res.status(200).json({ message: "Auth route placeholder" });
+// };
+
+// Temporary workaround
+export default (req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { message: string; }): void; new(): any; }; }; }) => {
   res.status(200).json({ message: "Auth route placeholder" });
 };
