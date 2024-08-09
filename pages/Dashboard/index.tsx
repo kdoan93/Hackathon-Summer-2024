f75c2ff962c63cfc28b9dff73253ce32dca80840
Kyrene <Kyrene.Aflores@gmail.com>
2024-08-09 18:20:54 -0500

    added Delete API

--- pages/Dashboard/index.tsx
+++ pages/Dashboard/index.tsx
@@ -7,19 +7,19 @@ const Dashboard = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
   return (
     <div className="dashboard-page">
       <NavBar isLoggedIn={isLoggedIn} />
-      <div className="container">
-        <Head>
-          <title>Dashboard | Sustain</title>
-        </Head>
-        <main>
-          <h1>Dashboard</h1>
-          {/* Dashboard content here */}
-          <DashboardPage />
-        </main>
-        <footer>
-          <p className="copyright">© 2024 Sustain</p>
-        </footer>
-      </div>
+      {/* <div className="container"> */}
+      <Head>
+        <title>Dashboard | Sustain</title>
+      </Head>
+      <main>
+        <h1>Dashboard</h1>
+        {/* Dashboard content here */}
+        <DashboardPage />
+      </main>
+      <footer>
+        <p className="copyright">© 2024 Sustain</p>
+      </footer>
+      {/* </div> */}
     </div>
   );
 };
