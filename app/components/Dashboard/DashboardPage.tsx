818563535c089b023837b69933a36fd4852c21ea
Kyrene <Kyrene.Aflores@gmail.com>
2024-08-09 19:34:54 -0500

    created table for dashboard

--- app/components/Dashboard/DashboardPage.tsx
+++ app/components/Dashboard/DashboardPage.tsx
@@ -16,7 +16,7 @@ const DashboardPage: React.FC = () => {
   const [error, setError] = useState<string | null>(null);
   const { user } = useUser();
   const userId = user?.id;
-
+  console.log("user", user);
   useEffect(() => {
     const loadData = async () => {
       if (!userId) {
@@ -43,12 +43,33 @@ const DashboardPage: React.FC = () => {
     loadData();
   }, [userId]);
 
-  // console.log("userData", userData);
+  console.log("userData1", userData);
 
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Meal</th>
+              <th>Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {userData?.data.map((info) => (
+              <tr key={info.id} className="bg-base-200">
+                <td>{info.createdAt}</td>
+                <td>{info.prompt}</td>
+                <td>{info.response?.Calories}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
