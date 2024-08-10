72dd8a2ae5ba62fb400010890b67518f9c8f782a
Kyrene <Kyrene.Aflores@gmail.com>
2024-08-10 17:11:33 -0500

    cleaned up code

--- app/components/Dashboard/DashboardPage.tsx
+++ app/components/Dashboard/DashboardPage.tsx
@@ -25,11 +25,9 @@ const DashboardPage: React.FC = () => {
   const [userData, setUserData] = useState<UserData[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [startDate, setStartDate] = useState<string>("");
-  const [endDate, setEndDate] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const { user } = useUser();
   const userId = user?.id;
-  console.log("user", user);
 
   // Below gets user data buy userId
   useEffect(() => {
@@ -58,61 +56,10 @@ const DashboardPage: React.FC = () => {
     loadData();
   }, [userId]);
 
-  console.log("userData1", userData);
-
-  //Below filters the data based on dates selected
-  const filterDataByWeek = (data: UserData[]) => {
-    if (!startDate || !endDate) return data;
-
-    const start = new Date(startDate).getTime();
-    const end = new Date(endDate).getTime();
-
-    return data.filter((item) => {
-      const itemDate = new Date(item.createdAt).getTime();
-      return itemDate >= start && itemDate <= end;
-    });
-  };
-
-  const filteredData = userData ? filterDataByWeek(userData.data) : [];
-
   return (
     <div className="dashboard-main flex flex-col items-center justify-center gap-5">
-      <Graph userData={{ data: filteredData }} />
+      <Graph userData={userData} />
 
-      {/* Date selection inputs*/}
-      <div className="flex space-x-4 my-4">
-        <div>
-          <label
-            htmlFor="start-date"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Start Date
-          </label>
-          <input
-            type="date"
-            id="start-date"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-          />
-        </div>
-
-        <div>
-          <label
-            htmlFor="end-date"
-            className="block text-sm font-medium text-gray-700"
-          >
-            End Date
-          </label>
-          <input
-            type="date"
-            id="end-date"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-          />
-        </div>
-      </div>
       {/* Calorie chart section */}
       <div className="overflow-x-auto">
         <table className="table">
@@ -138,11 +85,7 @@ const DashboardPage: React.FC = () => {
                 }
               );
               return (
-                <tr
-                  key={info.id}
-                  className="hover bg-base-200"
-                  onClick={() => setCurrMeal(info.response)}
-                >
+                <tr key={info.id} className="hover bg-base-200">
                   <td>{formattedDate}</td>
                   <td>{info.prompt}</td>
                   <td>{info.response?.Calories}</td>
