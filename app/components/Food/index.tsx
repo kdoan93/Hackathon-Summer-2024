c772d263c1fd9d1ea57bfc9bc07fb6c426b196d9
Kenny Doan <kdoan93@gmail.com>
2024-08-09 22:14:36 -0400

    Completed GET food logs for user

--- app/components/Food/index.tsx
+++ app/components/Food/index.tsx
@@ -1,50 +1,65 @@
+"use client";
 import { useUser } from "@clerk/nextjs";
-import { useEffect, useState } from "react"
-import { setEnvironmentData } from "worker_threads"
-import NavBar from "../NavBar/NavBar";
+import React, { useEffect, useState } from "react";
+import NutritionTable from "../NutritionTable/NutritionTable";
+// import '../Food/food.css'
 
-type Data = {
-    data: object;
-    userId: string;
+interface ResponseData {
+    [key: string]: string | number
+}
+
+interface LogData {
     prompt: string;
-    response: object;
-    createdAt: Date;
+    response: ResponseData;
+}
+
+interface Data {
+    success: boolean;
+    data: LogData[];
 }
 
 const Food = () => {
-    const [data, setData] = useState<Data[]>([])
-    const [error, setError] = useState<string | null>(null)
+    const [data, setData] = useState<LogData[]>([]);
+    const [error, setError] = useState<string | null>(null);
 
-    let { user } = useUser()
+    const { user } = useUser();
 
     useEffect(() => {
         const fetchData = async () => {
-            if (!user) return
-            const userNum = user.id
+            if (!user) return;
+            const userNum = user.id;
 
             try {
-                const response = await fetch(`/api/dashboard/getDashboard?userId=${userNum}`)
-                if (!response.ok) throw new Error(`Error: ${response.statusText}`)
+                const response = await fetch(`/api/dashboard/getDashboard?userId=${userNum}`);
+                if (!response.ok) throw new Error(`Error: ${response.statusText}`);
 
-                const result: Data[] = await response.json()
-                setData(result)
+                const result: Data = await response.json();
+                setData(result.data);
 
             } catch (error) {
-                setError((error as Error).message || "An error occurred")
+                setError((error as Error).message || "An error occurred");
             }
-        }
-        fetchData()
-    }, [user])
+        };
+        fetchData();
+    }, [user]);
 
-    let userLog = data.data
-    console.log("FOOD DATA: ", userLog)
+    let userLog = data;
+    console.log("FOOD DATA: ", userLog);
 
     return (
         <>
-            {/* <NavBar /> */}
-            <h1>User's food log</h1>
+            {userLog.length > 0 ? (
+                userLog.map((log, index) => (
+                    <div className="logContainer" key={index}>
+                        <div className="logPrompt" >{log.prompt}</div>
+                        <NutritionTable response={log.response} />
+                    </div>
+                ))
+            ) : (
+                <p>No data available</p>
+            )}
         </>
-    )
+    );
 }
 
-export default Food
+export default Food;
