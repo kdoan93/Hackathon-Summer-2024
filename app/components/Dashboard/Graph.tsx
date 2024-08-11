d60ac641b79bdd68667cacff979e25bbbb6d02d4
Kyrene <Kyrene.Aflores@gmail.com>
2024-08-11 13:24:51 -0500

    cleaned up code and removed console.logs

--- app/components/Dashboard/Graph.tsx
+++ app/components/Dashboard/Graph.tsx
@@ -24,21 +24,13 @@ interface UserData {
   data: InfoData[];
 }
 
-// interface GraphProps {
-//   _id: ObjectId;
-//   createdAt: Date;
-//   prompt: string;
-//   response: Object;
-//   userId: string;
-//   userData: UserData;
-// }
 interface GraphProps {
-  _id?: ObjectId; // Optional if not needed
-  createdAt?: Date; // Optional if not needed
-  prompt?: string; // Optional if not needed
-  response?: Object; // Optional if not needed
-  userId?: string; // Optional if not needed
-  userData?: UserData | null; // Allow null or undefined
+  _id?: ObjectId;
+  createdAt?: Date;
+  prompt?: string;
+  response?: Object;
+  userId?: string;
+  userData?: UserData | null;
 }
 
 ChartJS.register(
@@ -82,7 +74,7 @@ const Graph: React.FC<GraphProps> = ({ userData }) => {
       },
     },
   };
-  console.log("Graph prop", userData);
+
   return (
     <div className="flex justify-center w-full">
       <div className="w-full max-w-screen-lg h-[400px]">
